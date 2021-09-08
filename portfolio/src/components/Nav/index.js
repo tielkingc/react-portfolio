8a0c5c7ed49166b065ed4dd2723e775013196acc
Christian Tielking <tielking5@gmail.com>
2021-09-07 22:00:41 -0400

    started nav component

---
+++ portfolio/src/components/Nav/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <header className="flex-row px-1">
+            <h2>Christian Tielking</h2>
+        </header>
+    )
+}
+
+export default Nav;
